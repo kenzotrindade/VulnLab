c6962ca5faa47801be9592a3ad496d5909ab2c05
kenzotrindade <kenzo.trindade@gmail.com>
2026-02-19 14:52:29 +0100

    Add: FingerPrinting Scanner

--- backend/core/scanner.py
+++ backend/core/scanner.py
@@ -1,12 +1,13 @@
 import requests
 from .modules.headers import scanHeaders
 from .modules.files import scanFiles
+from .modules.fingerprinting import scanFingerprint
 
 def launchScanner(url: str):
   try:
     response = requests.get(url)
     if response.status_code == 200:
-      results = {"Header": scanHeaders(response.headers), "Files": scanFiles(url)}
+      results = {"Header": scanHeaders(response.headers), "Files": scanFiles(url), "FingerPrinting": scanFingerprint(response.headers)}
       return results
   except:
     return {"success": False, "errorMessage": "Error"}
